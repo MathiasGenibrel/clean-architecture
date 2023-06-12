1ac0e0948009b8afaace67fbf4c9a8110d36c4c6
MathiasGenibrel <mathias.geni@gmail.com>
2023-06-12 22:40:56 +0200

    ✨ Add post feature

--- index.ts
+++ index.ts
@@ -1,3 +1,55 @@
 #!/usr/bin/env node
+import { Command } from "commander";
+import {
+  DateProvider,
+  PostMessageCommand,
+  PostMessageUsecase,
+} from "./src/post-message.usecase";
+import { InMemoryMessageRepository } from "./src/inmemory-message.repository";
 
-console.log("Hello World !");
+const program = new Command();
+
+const messageRepository = new InMemoryMessageRepository();
+
+class RealDateProvider implements DateProvider {
+  getNow(): Date {
+    return new Date();
+  }
+}
+
+const dateProvider = new RealDateProvider();
+
+const postMessageUsecase = new PostMessageUsecase(
+  messageRepository,
+  dateProvider
+);
+
+program
+  .name("Tweet")
+  .description("Social to test clean architecture")
+  .version("1.0.0");
+
+program
+  .command("post")
+  .description("Create a new post on user timeline")
+  .argument("<user>", "The current user")
+  .argument("<message>", "The message to post")
+  .action((user, message) => {
+    const postMessageCommand: PostMessageCommand = {
+      id: "message-id",
+      text: message,
+      author: user,
+    };
+
+    try {
+      postMessageUsecase.handle(postMessageCommand);
+      console.log("✅  Message posté");
+      console.table([messageRepository.message]);
+    } catch (err) {
+      console.log("❌", err);
+    }
+  });
+
+const bootstrap = async () => await program.parseAsync();
+
+bootstrap();
